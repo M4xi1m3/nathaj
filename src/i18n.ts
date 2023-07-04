ec6c79c79eb187c705030ea4b02ca37e11b2c418
M4x1m3 <M4x1me@protonmail.com>
2023-07-04 11:47:54 +0200

    [frontend/i18n] Added language detector

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,4 +1,5 @@
 import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 import { en } from './translations/en';
 import { fr } from './translations/fr';
@@ -10,13 +11,15 @@ const resources = {
 
 export const availableLanguages = Object.keys(resources);
 
-i18n.use(initReactI18next).init({
-    resources,
-    lng: 'en',
-    lowerCaseLng: true,
-    interpolation: {
-        escapeValue: false,
-    },
-});
+i18n.use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        resources,
+        lowerCaseLng: true,
+        fallbackLng: 'en',
+        interpolation: {
+            escapeValue: false,
+        },
+    });
 
 export default i18n;
